e8bc53e41c27e0bbbe63257636f1f1587f843117
Himan1999 <cu.16bcs1194@gmail.com[B>
2022-11-13 09:04:15 +0530

    deletepost 2 to 4 ans

---
+++ promises.js
@@ -0,0 +1,125 @@
+const posts = [
+
+    { title: 'Post One', body: 'this is post one', createdAt: new Date().getTime() },
+
+    { title: 'Post two', body: 'this is post two', createdAt: new Date().getTime() }
+
+]
+
+let intervalId = 0;
+
+function getPost() {
+
+    clearInterval(intervalId);
+
+    setInterval(() => {
+
+        let output = '';
+
+        for (let i = 0; i < posts.length; i++) {
+
+            output += `<li>${posts[i].title} -
+
+            last updated ${(new Date().getTime() - posts[i].createdAt) / 1000} seconds ago 
+
+            </li>`;
+
+        }
+
+        document.body.innerHTML = output;
+
+    }, 1000)
+
+}
+
+
+
+function createPost(post) {
+
+    return new Promise((resolve, reject) => {
+
+        setTimeout(() => {
+
+            posts.push({ post, createdAt: new Date().getTime() });
+
+            const error = false;
+
+
+
+            if (!error) {
+
+                resolve();
+
+            } else {
+
+                reject('Something went wrong');
+
+            }
+
+        }, 2000)
+
+    })
+
+}
+
+
+
+function deletepost() {
+
+    return new Promise((resolve, reject) => {
+
+        setTimeout(() => {
+
+            if (posts.length > 0) {
+
+                const lastElement = posts.pop();
+
+                resolve(lastElement)
+
+            } else {
+
+                reject('Arrays is empty now');
+
+            }
+
+        }, 1000);
+
+
+
+    });
+
+}       
+
+createPost({ title: 'Post three', body: 'this is post three' })
+
+    .then(() => {
+
+        getPost()
+
+            deletepost().then(() => {
+
+                getPost();
+
+                deletepost().then(() => {
+
+                    getPost();
+
+                    deletepost().then(() => {
+
+                        getPost();
+
+                        deletepost().then(() => {
+
+                        }).catch((err) => {
+
+                            console.log('inside catch block',err)
+
+                        })
+
+                    })
+
+                })
+
+            })
+
+        })
